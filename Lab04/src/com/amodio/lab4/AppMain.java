53f9a44b97c0e10d1599f8a6faaaaf1db048f031
Lucca Amodio <lucca.amodio@gmail.com>
2023-05-11 07:29:01 -0300

    Alterações Lab04 11/05

--- Lab04/src/com/amodio/lab4/AppMain.java
+++ Lab04/src/com/amodio/lab4/AppMain.java
@@ -1,34 +1,213 @@
 package com.amodio.lab4;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class AppMain{
 	 
 	static Scanner scan = new Scanner(System.in);
-	
+	public static void main(String[] args) {
+		LinkedList<Cliente> listaPJ; //vão guardar as listas dos retornos futuros
+		LinkedList<Cliente> listaPF;
+		boolean sinistros_joao; //vão guardar os booleans para verificar se há sinistro para cada cliente
+		boolean sinistros_samsung;
+		boolean sinistros_google;
+		//Instanciação dos objetos
+		ClientePJ samsung = new ClientePJ("Samsung", "Avenida do Estado 221", "38.320.036/0001-25", "1975-08-06",100);
+		ClientePJ apple = new ClientePJ("Apple", "Avenida Paulista 123", "22.222.222/2222-22", "1965-02-03",500); //CNPJ inválido, apenas para testar
+		ClientePJ google = new ClientePJ ("Google", "Avenida Brasil 222", "15.202.005/0001-74", "1950-06-11", 700); //Cliente adicional que apenas será adicionado e removido da seguradora
+		ClientePF joao = new ClientePF("João", "Avenida 9 de Julho 281", "2023-02-17", "Médio Completo", "Masculino", "Classe B","427.638.700-07", "2003-04-11");
+		ClientePF maria = new ClientePF ("Maria", "Avenida Norte Sul 111", "2023-01-15", "Superior completo", "Feminino", "Classe A", "111.111.111-11", "1985-02-13"); //CPF inválido, apenas para testar
+		Veiculo focus = new Veiculo("5632", "Ford", "Focus",2010);
+		Veiculo civic = new Veiculo ("6974", "Honda", "Civic",2021);
+		Seguradora seguradora = new Seguradora ("Porto Seguro", "32569878", "portoseguro@gmail.com","Avenida Paulista 100");
+		Seguradora.adicionaSeguradora(seguradora);
+		//Teste dos CPFs e CNPJs
+		if(ClientePJ.validarCNPJ(samsung.getCnpj())) {
+			System.out.println("O CNPJ da Samsung é válido");
+		} else {
+			System.out.println("O CNPJ da Samsung é inválido");
+		}
+		
+		if(ClientePJ.validarCNPJ("22.222.222/2222-22")) { //passando a string do CNPJ, pois o campo CNPJ do objeto apple é null por ser inválido
+			System.out.println("O CNPJ da Apple é válido");
+		} else {
+			System.out.println("O CNPJ da Apple é inválido");
+		}
+		if(ClientePJ.validarCNPJ(google.getCnpj())) {
+			System.out.println("O CNPJ da Google é válido");
+		} else {
+			System.out.println("O CNPJ da Google é inválido");
+		}
+		if(ClientePF.validarCPF(joao.getCpf())) {
+			System.out.println("O CPF do João é válido");
+		} else {
+			System.out.println("O CPF do João é inválido");
+		}
+		
+		if(ClientePF.validarCPF("111.111.111-11")) { //passando a string do CPF, pois o campo CPF do objeto maria é null por ser inválido
+			System.out.println("O CPF da Maria é válido");
+		} else {
+			System.out.println("O CPF da Maria é inválido");
+		}
+		
+		//Impressão dos toString
+		System.out.println("Samsung:");
+		System.out.println(samsung);
+		System.out.println("João:");
+		System.out.println(joao);
+		System.out.println("Apple:");
+		System.out.println(apple); //CNPJ é null
+		System.out.println("Maria:");
+		System.out.println(maria); //CPF é null
+		System.out.println("Focus:");
+		System.out.println(focus);
+		System.out.println("Civic:");
+		System.out.println(civic);
+		System.out.println("Seguradora:");
+		System.out.println(seguradora);
+		System.out.println("Google:");
+		System.out.println(google);
+		
+		//Cadastros de veículos e clientes
+		if(!samsung.cadastraVeiculo(focus)) {
+			System.out.println("Veículo já cadastrado");
+		}
+		if(!joao.cadastraVeiculo(civic)){
+			System.out.println("Veículo já cadastrado");
+		}
+		if(!seguradora.cadastrarCliente(samsung)){
+			System.out.println("Cliente já cadastrado ou cliente inválido");
+		}
+		if(!seguradora.cadastrarCliente(joao)) {
+			System.out.println("Cliente já cadastrado ou cliente inválido");
+		}
+		System.out.println("Tentando cadastrar Maria:");
+		if(!seguradora.cadastrarCliente(maria)) { //Tem que dar mensagem de erro ao cadastrar, pois CPF é null
+			System.out.println("Cliente já cadastrado ou cliente inválido");
+		}
+		if(!seguradora.cadastrarCliente(google)) {
+			System.out.println("Cliente já cadastrado ou cliente inválido");
+		}
+		//Imprimindo a lista de veículos de Samsung e João
+		System.out.println("Veículos de Samsung:");
+		samsung.imprimeListaVeiculos(samsung.getListaVeiculos());
+		System.out.println("Veículos de João:");
+		samsung.imprimeListaVeiculos(joao.getListaVeiculos());
+		
+		//Testando o método de listar Clientes
+		System.out.println("Clientes PJ da Seguradora:");
+		listaPJ = seguradora.listarClientes("ClientePJ");
+		seguradora.imprime_listaCliente(listaPJ);
+		
+		System.out.println("Clientes PF da Seguradora:");
+		listaPF = seguradora.listarClientes("ClientePF");
+		seguradora.imprime_listaCliente(listaPF);
+		
+		System.out.println("Todos os clientes da seguradora:");
+		seguradora.imprime_listaCliente(seguradora.getListaClientes());
+		
+		//Gerando sinistros
+		
+		if(!seguradora.gerarSinistro("2022-08-09", "Avenida Paulista 272", seguradora, joao, civic)) {
+			System.out.println("Erro ao gerar o sinistro: Verifique se o cliente é da seguradora");
+		}
+		if(!seguradora.gerarSinistro("2021-07-24", "Avenida Carlos Grimaldi 210", seguradora, samsung, focus)) {
+			System.out.println("Erro ao gerar o sinistro: Verifique se o cliente é da seguradora");
+		}
+		
+		//visualização dos sinistros:
+		System.out.println("Sinistros da seguradora:");
+		seguradora.listarSinistros();
+		System.out.println("Visualizando os sinistos de João: ");
+		sinistros_joao = seguradora.visualizarSinistro("João"); //A impressão dos sinistros ocorre no método
+		if (!sinistros_joao) { //se retorna false, não há sinistros nesse nome
+			System.out.println("João não possui sinistros");
+		}
+		System.out.println("Visualizando os sinistos de Samsung: ");
+		sinistros_samsung = seguradora.visualizarSinistro("Samsung");
+		if (!sinistros_samsung) {
+			System.out.println("Samsung não possui sinistros");
+		}
+		System.out.println("Visualizando os sinistos da Google: ");
+		sinistros_google = seguradora.visualizarSinistro("Google");
+		if (!sinistros_google) {
+			System.out.println("Google não possui sinistros"); //deve imprimir isso, pois não há sinistro cadastrado para a Google
+		}
+		//Removendo Google da seguradora
+		seguradora.removerCliente("Google");
+		System.out.println("Todos os clientes da seguradora após remover Google:");
+		seguradora.imprime_listaCliente(seguradora.getListaClientes()); //Google não aparece mais na lista de Clientes
+		listaPJ = seguradora.listarClientes("ClientePJ");
+		System.out.println("Clientes PJ da seguradora:");
+		seguradora.imprime_listaCliente(listaPJ);//Google não aparece mais na lista de Clientes PJ
+		/*
+		//Implementação do método de leitura de dados
+		Scanner scan = new Scanner(System.in);
+        	System.out.println("Escolha alguma das opções de visualização abaixo:");
+		System.out.println("Para listar os sinistros da seguradora, digite 1.");
+		System.out.println("Para listar os clientes PF da seguradora, digite 2.");
+		System.out.println("Para listar os clientes PJ, digite 3.");
+		System.out.println("Para listar os sinistros de João, digite 4.");
+		System.out.println("Para listar os sinistros de Samsung, digite 5.");
+		System.out.println("Digite sua entrada:");
+		String entrada = scan.next(); //faz a leitura
+
+		if(entrada.equals("1")) { //se entrada for 1
+			seguradora.listarSinistros(); //lista os sinistros
+		} else if(entrada.equals("2")) { //se entrada for 2
+			listaPF = seguradora.listarClientes("ClientePF"); //lista os clientes PF
+			seguradora.imprime_listaCliente(listaPF);
+		}else if(entrada.equals("3")){ //se entrada for 3
+			listaPJ = seguradora.listarClientes("ClientePJ"); //lista os clientes PJ
+			seguradora.imprime_listaCliente(listaPJ);
+		}else if(entrada.equals("4")) { //se entrada for 4
+			 sinistros_joao = seguradora.visualizarSinistro("João"); //visualiza os sinistros de João
+			if (!sinistros_joao) {
+				System.out.println("João não possui sinistros");
+			}
+		} else if(entrada.equals("5")) { //se entrada for 5
+			 sinistros_samsung = seguradora.visualizarSinistro("Samsung"); //visualiza sinistros de Samsung
+			if (!sinistros_samsung) {
+				System.out.println("Samsung não possui sinistros");
+			}
+		} else { //se não for nenhuma das opções, mensagem de erro
+			 System.out.println("Entrada inválida");
+		}
+
+		scan.close(); //fecha o scan
+		System.out.println(joao.calcula_idade());
+		System.out.println(seguradora.calcularReceita());
+		System.out.println(samsung.getValorSeguro());
+		System.out.println(joao.getValorSeguro());
+		*/
+		
+		menu();
+	}
 	public static void menu() {
 		String escolha, data, endereco;
+		int escolha2;
 		Seguradora seguradora;
 		Cliente cliente;
 		Veiculo veiculo;
 		System.out.println("ESCOLHA UMA DAS OPÇÕES:");
 		for(MenuOperacoes m: MenuOperacoes.values()) {
-			System.out.println(m);
+			System.out.println(m.valor + " - " + m);
 		}
-		
-		escolha = scan.nextLine(); 
-		MenuOperacoes m = MenuOperacoes.valueOf(escolha);
-		switch(m.operacao) {
-			case 1:
+		escolha2 = scan.nextInt();
+		scan.nextLine();
+		MenuOperacoes m = MenuOperacoes.busca(escolha2); //.busca(valor lido)
+		switch(m) {
+			case CADASTRAR:
 				cadastrar();
 				break;
-			case 2:
+			case LISTAR:
 				listar();
 				break;
-			case 3:
+			case EXCLUIR:
 				excluir();
 				break;
-			case 4:
+			case GERAR_SINISTRO:
 				System.out.println("Escolha uma data para o sinistro na forma yyyy-mm-dd:");
 				data = scan.nextLine();
 				System.out.println("Escolha um endereço para o sinistro:");
@@ -58,7 +237,7 @@ public class AppMain{
 					System.out.println("Erro ao gerar o sinistro");
 				}
 				break;
-			case 6:
+			case CALCULAR_RECEITA_SEGURADORA:
 				seguradora = escolherSeguradora();
 				if(seguradora == null) {
 					System.out.println("Seguradora não encontrada");
@@ -66,8 +245,10 @@ public class AppMain{
 				}
 				System.out.println(seguradora.calcularReceita());
 				break;
-			case 0:
+			case SAIR:
 				break;
+			default:
+				System.out.println("Escolha uma opção válida");
 		}
 		
 	}
@@ -141,15 +322,15 @@ public class AppMain{
 	public static Cliente escolherCliente(Seguradora seguradora) {
 		String escolha;
 		Cliente cliente;
-		System.out.println("Escolha uma cliente pelo nome correspondente:");
+		System.out.println("Escolha uma cliente pelo CPF correspondente:");
 		escolha = scan.nextLine();
 		cliente = seguradora.buscarCliente(escolha);
 		return cliente;
 	}
 /*
 	public Cliente criarCliente() {
-		String cpf,endereco, dataLicenca, educacao, genero,
-		classeEconomica,dataNascimento;
+		String nome, cpf,endereco, dataLicenca, educacao, genero,
+		classeEconomica,dataNascimento,escolha;
 		System.out.println("Escolha por cliente PF ou PJ(PF/PJ)");
 		escolha = scan.nextLine();
 		if(escolha == "PF") {
--- Lab04/src/com/amodio/lab4/MenuOperacoes.java
+++ Lab04/src/com/amodio/lab4/MenuOperacoes.java
@@ -1,21 +1,25 @@
 package com.amodio.lab4;
 
 public enum MenuOperacoes {
-	CADASTRAR (1) ,
-	LISTAR (2) ,
-	EXCLUIR(3),
-	GERAR_SINISTRO(4),
-	TRANSFERIR_SEGURO(5),
-	CALCULAR_RECEITA_SEGURADORA(6),
-	SAIR (0);
-	
-	public final int operacao ;
-	
-	MenuOperacoes ( int operacao ) {
-		this . operacao = operacao ;
+	CADASTRAR(1), LISTAR(2), EXCLUIR(3), GERAR_SINISTRO(4), TRANSFERIR_SEGURO(5), CALCULAR_RECEITA_SEGURADORA(6), SAIR(0);
+
+	final int valor;
+
+	MenuOperacoes(int valor) {
+		this.valor = valor;
 	}
 
-	public int getOperacao () {
-		return this . operacao ;
+	public int getValor() {
+		return this.valor;
+	}
+
+
+	public static MenuOperacoes busca(int valor) {
+		for (MenuOperacoes i : MenuOperacoes.values()) {
+			if (i.getValor() == valor) {
+				return i;
+			}
+		}
+		return null;
 	}
 }
